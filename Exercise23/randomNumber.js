86b42dec82e1630bd9395b49e4f69ae68e12f7fb
BranBautista <bbrandon.abo@gmail.com>
2022-12-09 10:43:49 -0600

    First

---
+++ Exercise23/randomNumber.js
@@ -0,0 +1,8 @@
+function generateRandomIntegerNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(generateRandomIntegerNum(1, 100));
+console.log(generateRandomIntegerNum(1, 100));
+console.log(generateRandomIntegerNum(1, 100));
+console.log(generateRandomIntegerNum(1, 100));
